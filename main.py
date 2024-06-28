4e972eac9a319c30501b6d7da6a857a0206a0470
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-27 18:06:32 -0700

    First

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
     total = 0
+    i = 0
+    while i != 5:
+        num = int(input('Enter an integer'))
+        total += num
+        i += 1
+    print(total)
 
     ########################################
     # Do not delete the return statement
